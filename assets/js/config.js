b0d2e58cf713f82122830d51478c6ba830e26b07
erdder1103-art <erdder1103@gmail.com>
2026-06-05 15:46:15 +0800

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-window.LANDING_CONFIG = {
-  PIXEL_ID: '911833795220495',
-  LINE_URL: 'https://line.me/R/ti/p/@h1788',
-  TWITCH_URL: 'https://www.twitch.tv/vip1788',
-  CLICK_DELAY_MS: 3000,
-  LOADING_SECONDS: 3
-};
